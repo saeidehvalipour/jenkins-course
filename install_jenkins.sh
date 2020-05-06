59378bd2ae888143e2340c554ebbd4138910de8f
saeideh valipour <sv222fi@student.lnu.se>
2020-05-06 19:11:22 +0200

    refactoring1

--- install_jenkins.sh
+++ install_jenkins.sh
@@ -4,7 +4,7 @@
 
 # install docker
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository 
+sudo add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu 
    $(lsb_release -cs) 
    stable"
