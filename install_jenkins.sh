30c0e7a3747bd30a6ede76852b4dfc7bbf5c5dc1
saeideh valipour <sv222fi@student.lnu.se>
2020-05-06 19:29:29 +0200

    keep refactoring

--- install_jenkins.sh
+++ install_jenkins.sh
@@ -4,10 +4,7 @@
 
 # install docker
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 apt-get update
 apt-get install docker-ce docker-ce-cli containerd.io
 systemctl enable docker
