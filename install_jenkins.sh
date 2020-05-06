50b3c39f242829fae5e374c1dc02a11e04678c46
saeideh valipour <sv222fi@student.lnu.se>
2020-05-06 19:08:05 +0200

    refactoring

--- install_jenkins.sh
+++ install_jenkins.sh
@@ -1,13 +1,12 @@
-  
 #!/bin/bash
 
 # this script is only tested on ubuntu xenial
 
 # install docker
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
+sudo add-apt-repository 
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu 
+   $(lsb_release -cs) 
    stable"
 apt-get update
 apt-get install docker-ce docker-ce-cli containerd.io
